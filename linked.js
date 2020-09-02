ef99218c14aaf2160efc4053306668df10f1463a
Fahad Tufail <36541768+fahadtufail007@users.noreply.github.com>
2020-09-02 21:10:16 +0500

    added findUnlinkedUsers method

--- linked.js
+++ linked.js
@@ -1,1 +1,10 @@
 // find all the users that have not been linked
+import { allUsers, linked } from './users';
+
+function findUnlinkedUsers() {
+  return allUsers.filter(({ id: userId }) => {
+    return !linked.find(({ id: linkedId }) => {
+      return linkedId === userId;
+    });
+  });
+}
